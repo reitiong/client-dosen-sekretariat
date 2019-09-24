62c88ae61a7532a27bce169881409789ac7f91d6
feryadialoi <37759681+feryadialoi@users.noreply.github.com>
2019-09-24 21:58:26 +0700

    add chart

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -2,10 +2,20 @@ import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { NavLink } from "react-router-dom";
 import logo from "./../../assets/images/logo.png";
+import { PieChart, Pie, Sector, Cell, Tooltip } from "recharts";
+
 const Home = props => {
   useEffect(() => {
     // props.history.push("/login");
   });
+  const data = [
+    { name: "Group A", value: 400 },
+    { name: "Group B", value: 300 },
+    { name: "Group C", value: 300 },
+    { name: "Group D", value: 200 }
+  ];
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
   const matakuliah = [
     { mataKuliah: "Kalkulus" },
     { mataKuliah: "Kalkulus" },
@@ -40,6 +50,25 @@ const Home = props => {
         <div className="main-content p-3 bg-light">
           <div className="content-content p-2 border border-dark bg-light">
             <div>
+              <PieChart width={400} height={400}>
+                <Pie
+                  data={data}
+                  cx={200}
+                  cy={200}
+                  label
+                  outerRadius={80}
+                  fill="#8884d8"
+                  dataKey="value"
+                >
+                  {data.map((entry, index) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={COLORS[index % COLORS.length]}
+                    />
+                  ))}
+                </Pie>
+                <Tooltip />
+              </PieChart>
               Senin
               <table className="table table-striped table-hover">
                 <thead className="thead-dark">
